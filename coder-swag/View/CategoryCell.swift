758a9433b74cae6bf24570ba037b6976beb743fd
Daniel Bendemagh <daniel.bendemagh@laposte.net>
2018-06-22 21:20:11 +0200

    Add Navigation controller and Table View

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Daniel BENDEMAGH on 22/06/2018.
+//  Copyright © 2018 Daniel BENDEMAGH. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
