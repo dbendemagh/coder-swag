7bc34bd4cb36bd43491c847375aa9ef23857b27a
Daniel Bendemagh <daniel.bendemagh@laposte.net>
2018-06-24 12:15:52 +0200

    Add Collection View

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Daniel BENDEMAGH on 23/06/2018.
+//  Copyright © 2018 Daniel BENDEMAGH. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
